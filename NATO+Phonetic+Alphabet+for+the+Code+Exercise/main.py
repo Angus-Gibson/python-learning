e7f1af7c02c8e68cf2c30af22af1162c804e0651
Angus-Gibson <taylorgibson@msn.com>
2026-04-26 22:28:00 -0500

    Added NATO Phoenetic program with raised KeyError Exceptions.

---
+++ NATO+Phonetic+Alphabet+for+the+Code+Exercise/main.py
@@ -0,0 +1,18 @@
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+
+word = input("Enter a word: ").upper()
+while True:
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+        print(output_list)
+        break
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    word = input("Enter a word: ").upper()
+    
