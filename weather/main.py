f0574b15d4191e423edae096a5caabdb8a8ede75
Angus-Gibson <taylorgibson@msn.com>
2026-08-26 16:08:54 -0500

    put secrets in .env file and load them with dotenv

--- weather/main.py
+++ weather/main.py
@@ -1,6 +1,7 @@
+import os
+
 import requests
 from twilio.rest import Client
-import os
 
 # MY_LAT = 41.878113
 # MY_LONG = -87.629799
@@ -8,12 +9,14 @@ import os
 MY_LAT = 24.7
 MY_LONG = 134.22
 
-account_sid = "REDACTED_TWILIO_SID"
-auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
+ACCOUNT_SID = os.environ.get("TWILIO_SID")
+AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
+MY_PHONE = os.environ.get("MY_PHONE")
+TWILIO_PHONE = os.environ.get("TWILIO_PHONE")
+CONTENT_SID = os.environ.get("CONTENT_SID")
 
 OWM_API_KEY = os.environ.get("OWM_API_KEY")
 parameters = {
-
     "lat": MY_LAT,
     "lon": MY_LONG,
     "appid": OWM_API_KEY,
@@ -21,18 +24,22 @@ parameters = {
     "cnt": 4,
 }
 
-response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response = requests.get(
+    "https://api.openweathermap.org/data/2.5/forecast",
+    params=parameters,
+    timeout=10,
+)
 response.raise_for_status()
 data = response.json()
 
 for forecast in data["list"]:
     if forecast["weather"][0]["id"] < 700:
-        client = Client(account_sid, auth_token)
+        client = Client(ACCOUNT_SID, AUTH_TOKEN)
         message = client.messages.create(
-            to=f"whatsapp:{os.environ.get('MY_PHONE_NUMBER')}",
-            from_="whatsapp:+17372583478",
-            content_sid="HXfe5ab5f00277942d4d4200328b4d403c",
+            to=f"whatsapp:{MY_PHONE}",
+            from_=f"whatsapp:{TWILIO_PHONE}",
+            content_sid=CONTENT_SID,
         )
 
-        print(message.status)   
+        print(message.status)
         break
